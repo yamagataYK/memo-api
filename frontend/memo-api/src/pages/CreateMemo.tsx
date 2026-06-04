c04b872a0b62bf9ddc4ff0b31c90cfe477652349
Yamagata Kaito <yamagatakaito0921@gmail.com>
2026-06-04 11:49:58 +0900

    HTTP通信POST

--- frontend/memo-api/src/pages/CreateMemo.tsx
+++ frontend/memo-api/src/pages/CreateMemo.tsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
 
 
 function CreateMemo() {
+    const [text, setText] = useState('')
+    const [content, setContent] = useState('')
 
+    const handleClick = () => {
+        fetch('https://click.ecc.ac.jp/ecc/kyamagata/memo-api/backend/',
+            {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    title: (text),
+                    content: (content)
+                })
+            })
+            .then((response) => response.json())//ここでBodyからJSONを返す
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((e) => {
+                console.log(e)  //エラーをキャッチし表示     
+            })
+    };
 
     return (
-        <p>メモ作成</p>
-        
-        <input type="text" id="title" />
+        <>
+            <p>メモ作成</p>
+            <label htmlFor="title">タイトル</label>
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+
+            <textarea name="" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+
+            <button type="submit" onClick={handleClick} >保存</button >
+
+            <p>{text}</p>
+            <p>{content}</p>
+
+        </>
 
     )
 }
